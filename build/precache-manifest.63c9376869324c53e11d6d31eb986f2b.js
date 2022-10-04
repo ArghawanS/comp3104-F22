2bfc3c07a7471e747ebf3a97e652d15c1876a75e
ArghawanS <97705406+ArghawanS@users.noreply.github.com>
2022-10-03 23:58:06 -0400

    React project code added

---
+++ build/precache-manifest.63c9376869324c53e11d6d31eb986f2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "14ae57623f8af4f9dbd6",
+    "url": "/ArghawanS/comp3104-F22/static/css/main.2b081c46.chunk.css"
+  },
+  {
+    "revision": "14ae57623f8af4f9dbd6",
+    "url": "/ArghawanS/comp3104-F22/static/js/main.14ae5762.chunk.js"
+  },
+  {
+    "revision": "872c788845819f04512c",
+    "url": "/ArghawanS/comp3104-F22/static/js/runtime~main.872c7888.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/ArghawanS/comp3104-F22/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "52b3ee03b324b180d778911704bec045",
+    "url": "/ArghawanS/comp3104-F22/index.html"
+  }
+];
